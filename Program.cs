c37455bd4e04e989bb46264ba87e93a5f72b827f
raihan <raihanuddin21@gmail.com>
2020-03-21 21:03:51 +0600

    Initial

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace IncrementFileNameIfExists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string originalFileName = "TestFile";
+            string generatedFileName;
+
+            int count = 1;
+            do
+            {
+                generatedFileName = $"{originalFileName}({count}).txt";
+                count++;
+            } while (File.Exists(generatedFileName));
+
+            //Add your logic here
+            File.Create(generatedFileName);
+
+            Console.WriteLine($"{generatedFileName} created successfully!!!");
+            Console.ReadLine();
+        }
+    }
+}
